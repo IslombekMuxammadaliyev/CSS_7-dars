6964988f332e12fecc9423ccaceb609081058538
uzb077 <islombekmuxammadaliyev07@gmail.com>
2023-05-22 16:16:14 +0500

    Bu CSS 7-dars kodlari

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+let wrap = document.getElementsByTagName("div").namedItem("wrap")
+let fromgroup = document.getElementsByTagName("div").namedItem("fromgroup")
+let label1 = document.getElementsByTagName("label").namedItem("label1")
+let label2 = document.getElementsByTagName("label").namedItem("label2")
+let label3 = document.getElementsByTagName("label").namedItem("label3")
+let label4 = document.getElementsByTagName("label").namedItem("label4")
+let label5 = document.getElementsByTagName("label").namedItem("label5")
+let label6 = document.getElementsByTagName("label").namedItem("label6")
+let firstName = document.getElementsByTagName("input").namedItem("firstName")
+
+
+wrap.style.width = "1200px";
+wrap.style.margin = "50px auto";
+
+fromGroup.style.width = "500px";
+fromGroup.style.margin = "0 auto";
+fromGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)";
+fromGroup.style.padding = "1rem 1.2rem";
+fromGroup.style.boxSizing = "border-box";
+
+
+
+
+
+
+
